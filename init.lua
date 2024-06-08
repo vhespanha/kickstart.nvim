bf4124dd001c7bb34fb94c644d631000ebbcdacb
vhespanha <vhespanha@tuta.io>
2024-06-08 16:36:45 -0300

    feat: Getting good at this
    Added a lot of stuff to lualine, poorly ported my theme to it and swithced to octicons

--- init.lua
+++ init.lua
@@ -778,7 +778,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'github_dark_default'
+      vim.cmd.colorscheme 'dngh_dark_default'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,11 +1,113 @@
-return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
-  opts = {
-    options = {
-      icons = true,
+local copilot_status = function()
+    return require("copilot_status").status_string()
+end
+
+local colors = {
+    bg     = '#0f0f0f',
+    fg     = '#6a737d',
+
+    blue   = '#79b8ff',
+    green  = '#85e89d',
+    red    = '#f85149',
+    violet = '#b392f0',
+}
+
+local lualine_dngh = {
+    normal = {
+        a = { fg = colors.violet, bg = colors.bg },
+        b = { fg = colors.fg, bg = colors.bg },
+        c = { fg = colors.fg, bg = colors.bg },
+        z = { fg = colors.fg, bg = colors.bg },
     },
-  },
+
+    insert = { a = { fg = colors.blue, bg = colors.bg } },
+    visual = { a = { fg = colors.green, bg = colors.bg } },
+    replace = { a = { fg = colors.red, bg = colors.bg } },
+
+    inactive = {
+        a = { fg = colors.fg, bg = colors.bg },
+        b = { fg = colors.fg, bg = colors.bg },
+        c = { fg = colors.fg, bg = colors.bg },
+        z = { fg = colors.fg, bg = colors.bg },
+    },
+}
+
+return {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+        "nvim-tree/nvim-web-devicons"
+    },
+    -- See `:help lualine.txt`
+    opts = {
+        extensions = { 'nvim-tree', 'trouble', 'toggleterm' },
+        options = {
+            disabled_filetypes = { 'packer', 'NVimTree', 'NvimTree_1', 'quickfix', 'prompt', 'lazy', 'symbols-outline' },
+            icons_enabled = true,
+            theme = lualine_dngh,
+            component_separators = '',
+            section_separators = '|',
+        },
+        sections = {
+            lualine_a = {
+                {
+                    'mode',
+                    fmt = function()
+                        -- Define the icons for each mode
+                        local mode_icons = {
+                            n = ' ', -- Normal mode
+                            i = ' ', -- Insert mode
+                            v = ' ', -- Visual mode
+                            [''] = ' ', -- Visual block mode (requires special handling)
+                            V = ' ', -- Visual line mode
+                            c = ' ', -- Command mode
+                            R = ' ', -- Replace mode
+                            t = ' ', -- Terminal mode
+                        }
+
+                        -- Get the current mode
+                        local mode = vim.fn.mode()
+
+                        -- Return the corresponding icon
+                        return mode_icons[mode] or mode:sub(1, 1) -- Fallback to first character of mode if no icon found
+                    end
+                }
+            },
+            lualine_b = { "filename",
+                {
+                    'branch', icon = ' ',
+                }
+            },
+            lualine_c = {},
+            lualine_x = {
+                copilot_status,
+            },
+            lualine_y = {
+                {
+                    'filetype',
+                    colored = false,
+                    icon_only = true,
+                }
+            },
+            lualine_z = {
+                {
+                    'diagnostics',
+
+                    -- Table of diagnostic sources, available sources are:
+                    --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
+                    -- or a function that returns a table as such:
+                    --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+                    sources = { 'nvim_diagnostic', 'coc' },
+
+                    -- Displays diagnostics for the defined severity types
+                    sections = { 'error', 'warn', 'hint' },
+
+                    symbols = { error = ' ', warn = ' ', hint = ' ' },
+                    colored = false,         -- Displays diagnostics status in color if set to true.
+                    update_in_insert = true, -- Update diagnostics in insert mode.
+                    always_visible = true,   -- Show diagnostics even if there are none.
+                }
+            },
+        },
+    },
 }
